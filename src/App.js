4847afd3522c50360811d56cbac1e96e0951a66f
Taras Rushchak <taraskhust@gmail.com>
2019-05-20 11:50:56 +0300

    react hooks API

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Search from './search';
 
 function Pet() {
   return (
+      <React.StrictMode>
     <>
       <h1>Welcome</h1>
       <Search />
     </>
+      </React.StrictMode>
   );
 }
 
--- src/search.js
+++ src/search.js
@@ -1,32 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { ANIMALS } from '@frontendmasters/pet';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 
 const Search = () => {
   const [location, setLocation] = useState('');
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animals', 'dog', ANIMALS);
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+    setPets(animals || []);
+    console.log(animals)
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed('');
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedstrings = breeds.map(({ name }) => name);
+      setBreeds(breedstrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
   return (
-    <>
-      <div className="search-params">
-        <form style={{ display: 'flex', flexDirection: 'column' }}>
-          <label htmlFor="location">
-            Location
-            <input
-              type="text"
-              value={location}
-              placeholder={location}
-              onChange={event => setLocation(event.target.value)}
-            />
-          </label>
-          <AnimalDropdown />
-          <BreedDropdown />
-          <button>Submit</button>
-        </form>
-      </div>
-    </>
+    <div className="search-params">
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={event => {
+          event.preventDefault();
+          requestPets()
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            value={location}
+            placeholder={location}
+            onChange={event => setLocation(event.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
